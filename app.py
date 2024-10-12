a15660dd7acb5a7658053921a39b2a247a3e60f0
Andriy Fedirko <fedirko816@gmail.com>
2024-10-12 20:58:02 +0300

    Initial commit: created Flask app and set up env

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 from flask import Flask
+app = Flask(__name__)
 
-def create_app():
-    app = Flask(__name__)
-
-    @app.route('/')
-    def main():
-        return "Hello World!"
-
-    return app
-
-app = create_app()
+@app.route('/')
+def main():
+    return 'Hello, world!'
 
 if __name__ == '__main__':
     app.run(debug=True)
