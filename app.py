9d7b89b99f62fd1300cd21ddf8bc640b61e2e001
Andriy Fedirko <fedirko816@gmail.com>
2024-10-15 17:40:50 +0300

    add homepage

--- app.py
+++ app.py
@@ -10,5 +10,12 @@ def home():
     """View for the Home page of your website."""
     return f"<h1>This is your homepage :) </h1>"
 
+@app.route('/hi/<string:name>/<int:age>')
+def greetings(name, age):
+    name = name.upper()
+    year = 2024 - age
+    return f"Welcome {name=} {year=}"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
