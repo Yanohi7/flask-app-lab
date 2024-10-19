80303477ba83e13585300a26fb11c92ceb0fe41e
Yanohi7 <viktor.khudaska.22@pnu.edu.ua>
2024-10-19 13:32:57 +0300

    base html template created and "resume" page with routing is added

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from tempfile import template
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +11,10 @@ def main():
 @app.route('/homepage')
 def home():
     return f"This is your homepage :) "
+
+@app.route('/resume')
+def get_resume():
+    return render_template("resume.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
