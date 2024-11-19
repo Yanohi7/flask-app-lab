f5963dd944b579055b0fd3c5ea644652690b60d9
Yanohi7 <viktor.khudaska.22@pnu.edu.ua>
2024-11-19 21:17:14 +0200

    adding post button and code refactoring

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-import secrets
-app = Flask(__name__)
-app.config.from_pyfile("../config.py")
-app.config['SECRET_KEY'] = secrets.token_urlsafe(30)
 
-from . import views
-
-from .posts import post_bp
-from .users import bp as user_bp
-app.register_blueprint(post_bp)
-app.register_blueprint(user_bp, url_prefix="/users")
+def create_app(config_name='config'):
+    app = Flask(__name__)
+    app.config.from_object(config_name) #налаштування з файлу config.py
+    with app.app_context():
+        from . import views
+        from .posts import post_bp
+        from .users import user_bp
+        app.register_blueprint(post_bp)
+        app.register_blueprint(user_bp)
+        return app
--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 
-bp = Blueprint("user_name", 
+user_bp = Blueprint("user_name",
                __name__, 
                template_folder="templates/users",
                #url_prefix="/users2"
                )
 
 from . import views
+
